c1317b245acde446c19199ac5154fd5ad5663917
Sai Varshitha <velma_saivarshitha@epam.com>
2025-03-10 14:33:08 +0530

    commit

--- task03/jsrc/main/java/com/task03/HelloWorld.java
+++ task03/jsrc/main/java/com/task03/HelloWorld.java
@@ -23,6 +23,5 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 		resultMap.put("statusCode", 200);
 		resultMap.put("message", "Hello from Lambda");
 		return resultMap;
-
 	}
 }
